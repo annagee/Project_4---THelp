41894bebe85a75386e1f52c2a9ee7e3bb0d3eaac
Anna Gaughan <agaughan@gmail.com>
2015-07-09 09:50:26 +0100

    setting up folders etc

---
+++ db/migrate/20150708205703_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :content
+      t.string :author
+      t.string :time
+
+      t.timestamps null: false
+    end
+  end
+end
