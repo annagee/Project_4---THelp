9bfdb287f37a17bf6a8f373d482816dee0da56c0
Anna Gaughan <agaughan@gmail.com>
2015-07-09 22:08:02 +0100

    adding user to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,8 @@ class CommentsController < ApplicationController
  def create
   @question = Question.find(params[:question_id])
   @comment = @question.comments.create(params[:comment].permit(:comment))
- 
+  @comment.user_id = current_user.id if current_user
+
   if @comment.save
     redirect_to question_path(@question)
   else
