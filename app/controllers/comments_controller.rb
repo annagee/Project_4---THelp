739ef35c9d5181ac26de8d259b4c82509c197269
Anna Gaughan <agaughan@gmail.com>
2015-07-09 22:52:39 +0100

    adding edit comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,39 @@
 class CommentsController < ApplicationController
 
- def create
-  @question = Question.find(params[:question_id])
-  @comment = @question.comments.create(params[:comment].permit(:comment))
-  @comment.user_id = current_user.id if current_user
-  @comment.save
+   def create
+    @question = Question.find(params[:question_id])
+    @comment = @question.comments.create(params[:comment].permit(:comment))
+    @comment.user_id = current_user.id if current_user
+    @comment.save
 
-  if @comment.save
+    if @comment.save
+      redirect_to question_path(@question)
+    else
+      render 'new'
+    end
+   end
+
+   def edit
+   @question = Question.find(params[:question_id])
+    @comment = @question.comments.find(params[:id])
+   end
+
+   def update
+    @question = Question.find(params[:question_id])
+    @comment = @question.comments.find(params[:id])
+
+    if @comment = @question.comments.find(params[:id])
+      redirect_to question_path(@question)
+     else
+      render 'edit'
+    end  
+   end
+
+
+   def destroy
+    @question = Question.find(params[:question_id])
+    @comment = @question.comments.find(params[:id])
+    @comment.destroy
     redirect_to question_path(@question)
-  else
-    render 'new'
-  end
- end
-
- def destroy
-  @question = Question.find(params[:question_id])
-  @comment = @question.comments.find(params[:id])
-  @comment.destroy
-  redirect_to question_path(@question)
-end
+   end
 end
