efbb65a230dfb348dc5c4e94f28945ebac9a042e
Anna Gaughan <agaughan@gmail.com>
2015-07-09 16:24:18 +0100

    adding and displaying posts

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,32 @@
 class QuestionsController < ApplicationController
   def index
   end
+
+  def show
+     @question = Question.find(params[:id])
+
+    end
+
+  def new
+    @question = Question.new
+  end
+
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+  end
+end 
+
+  def edit
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
 end
