f0f1bb169ff9b187d8276cee1c8c2c5694d5e1ff
Anna Gaughan <agaughan@gmail.com>
2015-07-09 15:38:58 +0100

    adding questions model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :members
+  resources :questions
   root 'members#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709113113) do
+ActiveRecord::Schema.define(version: 20150709143545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "members", force: :cascade do |t|
     t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
