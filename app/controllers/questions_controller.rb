1d2147b2b94e17954ec694fe963bd5ffeae4ecd1
Anna Gaughan <agaughan@gmail.com>
2015-07-09 21:12:54 +0100

    adding authenication to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
    @question = Question.all.order("created_at DESC")
