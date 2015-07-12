c4b026c7debcad9b6a2e9c0d317b63ada4daa321
Anna Gaughan <agaughan@gmail.com>
2015-07-13 00:05:20 +0100

    adding search to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,22 @@
 class ArticlesController < ApplicationController
 
   def index
-   @article = Article.all
+   @articles = Article.all
   end
+  
+  def create
+    @article = Article.new(article_params)
+     respond_to do |format|
+      if @article.save
+        format.js
+      else
+      format.html{render root_path}  
+      end
+     end
+  end 
+
+  private
+   def article_params
+     params.require(:article).permit(:author, :header, :content, :all_tags)
+   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 class Article < ActiveRecord::Base
-  has_many :tags
+  has_many :article_tags
   has_many :tags, through: :article_tags
+
+  def all_tags=(names)
+    self.tags = names.split(", ").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+   end
+   
+   def all_tags
+     self.tags.map(&:name).join(", ") 
+    end 
 end
