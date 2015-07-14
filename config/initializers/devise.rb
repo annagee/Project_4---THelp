acd833ed58595aedddd049691238706c86af032b
Anna Gaughan <agaughan@gmail.com>
2015-07-14 11:05:50 +0100

    adding devise details for username but not working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,6 +4,12 @@ Devise.setup do |config|
   
   require "omniauth-facebook"
 
+
+  config.authentication_keys =[ :login ]
+
+  config.reset_password_keys = [ :username ]
+  config.confirmation_keys = [ :username ]
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -214,7 +220,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
