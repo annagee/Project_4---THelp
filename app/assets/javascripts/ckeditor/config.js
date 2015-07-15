d318d168af85d661626ca5899c91dccbbc038674
Anna Gaughan <agaughan@gmail.com>
2015-07-15 14:28:41 +0100

    commit before going back to master

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -8,3 +8,16 @@ CKEDITOR.editorConfig = function (config) {
 
   // ... rest of the original config.js  ...
 }
+
+RailsAdmin.config do |config|
+  config.model Team do
+    edit do
+      # For RailsAdmin >= 0.5.0
+      field :description, :ck_editor
+      # For RailsAdmin < 0.5.0
+      # field :description do
+      #   ckeditor true
+      # end
+    end
+  end
+end
