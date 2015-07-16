f3f258376f6a3dbe643d2df1cbb39b85705399af
Anna Gaughan <agaughan@gmail.com>
2015-07-16 19:15:44 +0100

    got truncate working on articles page

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,8 +7,6 @@ class Question < ActiveRecord::Base
    def all_tags=(names)
       self.tags=names.split(",").map do |name|
        Tag.where(name: name.strip).first_or_create!
-   
-
      end
    end
 
